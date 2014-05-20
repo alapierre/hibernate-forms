c9c4ae2016e5016c2f2f32f340e1e9960d5a5b0f
Adrian Lapierre <adrian@soft-project.pl>
2014-05-20 20:49:56 +0200

    poprawka - private na protected w Genercic DAO

--- src/main/java/pl/com/softproject/altkom/hibernate/dao/GenericDAOHibernateImpl.java
+++ src/main/java/pl/com/softproject/altkom/hibernate/dao/GenericDAOHibernateImpl.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2014-05-18 the original author or authors.
  */
 
 package pl.com.softproject.altkom.hibernate.dao;
